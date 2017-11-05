69642d3f340982c366bc8006bde876f48eee678c
Guxing0321 <guxing0321@163.com>
2017-11-05 17:22:29 +0800

    StructsTest

---
+++ src/main/java/cn/kekemile/action/TestAction.java
@@ -0,0 +1,23 @@
+package cn.kekemile.action;
+
+/**
+ * 
+ * test测试的Action
+ * 
+ * @author guxing
+ *
+ */
+public class TestAction {
+
+	/**
+	 * 瑕佹眰锛� 1.public 2.蹇呴』鏈夎繑鍥炲�硷紝杩斿洖鍊肩被鍨嬪繀椤讳负String
+	 * 3.鏂规硶鐨勫悕绉版槸浠绘剰鐨勶紝蹇呴』涓嶈兘甯﹀弬鏁�
+	 * 
+	 */
+	public String Test() {
+
+		System.out.println("Hello, Struts2!");
+		return "ok";
+	}
+
+}
